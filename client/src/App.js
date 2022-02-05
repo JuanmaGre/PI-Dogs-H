dfd756c333f361e126c4d8b988be99894d3f96a8
Juan Manuel <jmanuelgrehuello@yahoo.com>
2022-02-04 21:03:42 -0300

    3

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { landingPage } from "./compronents/landingPage";
+import { home } from "./compronents/home";
+import { dogCreate } from "./compronents/dogCreate";
+import { detail } from "./compronents/detail";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path = "/" component = { landingPage } />
+          <Route path = "/home" component = { home } />
+          <Route path = "/dog" component = { dogCreate } />
+          <Route path = "/dogs/:id" component = { detail } />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
